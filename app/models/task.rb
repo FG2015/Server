e404274930f5eeaf07e90c56444d1f03222cf1b2
Pedro <pepibumur@gmail.com>
2015-03-28 18:02:18 +0100

    Added task model\

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,22 @@
 class Task
   include Mongoid::Document
 
-  #field :last_sign_in_ip,    type: String
+  validates_presence_of :name
+  validates_presence_of :client
+  validates_presence_of :rma
+  validates_presence_of :name
+  validates_presence_of :name
+  validates_presence_of :name
+  validates_presence_of :name
+
+  # Fields
+  field :name,        type: String
+  field :client,      type: String
+  field :rma,         type: String
+  field :date,        type: Date
+  field :start_date,  type: Date
+  field :end_date,    type: Date
+  field :problem,     type: String
+  field :solution,    type: String
+  field :address,     type: String
 end
