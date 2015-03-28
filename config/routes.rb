046fb6266bf57f753ec8c245bff0a386b826265e
Pedro <pepibumur@gmail.com>
2015-03-28 17:22:59 +0100

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  #devise_for :users
   mount_devise_token_auth_for 'User', at: 'auth'
   # Root
   root to: "home#index"
