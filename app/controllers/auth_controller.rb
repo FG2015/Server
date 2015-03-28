6b82fd88645a2856c0d302f8ecd7bb3393b3c603
Pedro <pepibumur@gmail.com>
2015-03-28 17:50:11 +0100

    Some fixes

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,15 +5,18 @@ class AuthController < ApplicationController
     if !email
       render :json => { :status => :bad_request, :message => "Email required" }
       return
+    end
     password = params[:password]
     if !password
       render :json => { :status => :bad_request, :message => "Password required" }
       return
+    end
     # Checking credentials
     user = User.find_by(email: email)
     if !user
       render :json => { :status => :not_found, :message => "Not existing user" }
       return
+    end
     user.save # Force token generation
     if user.valid_password?(password)
       render :json => { :status => :ok, :token => user.authentication_token }
@@ -21,6 +24,7 @@ class AuthController < ApplicationController
     else
       render :json => { :status => :unauthorized, :message => "Invalid password" }
       return
+    end
   end
 
   def signup
